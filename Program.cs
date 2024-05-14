5852c35764a7f7913dec39dca0e036c177c34624
Amir <117419724+abaharloo@users.noreply.github.com>
2024-05-14 13:56:01 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Fibonachi
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter a number");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("The number in Fibonachi equals to: " + fib(number));
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+    public static int fib(int number)
+    {
+        int a = 0;
+        int b = 1;
+        for (int i = 1; i < number; i++)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return a;
+    }
+}
